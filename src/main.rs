3c6ca841531a933fac98024aa6677a19c29acbc6
Tim Yorke <tim.yorke@embeddedscience.co.uk>
2021-01-02 20:52:02 +0000

    10kHz software PWM output on GPIO pin

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
-use rppal::gpio::Gpio;
+use rppal::{gpio::Gpio, pwm};
+use rppal::pwm::{Channel, Polarity, Pwm};
 use std::{error::Error, thread::sleep, time::Duration};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut pin = Gpio::new()?.get(20)?.into_output();
-
     loop {
-        pin.set_high();
-        sleep(Duration::from_millis(10));
-        pin.set_low();
-        sleep(Duration::from_millis(90));
+        pin.set_pwm(
+            Duration::from_micros(100),
+            Duration::from_micros(25)
+        )?;
+
+        sleep(Duration::from_secs(1));
+
+        pin.set_pwm(
+            Duration::from_micros(100),
+            Duration::from_micros(50)
+        )?;
+
+        sleep(Duration::from_secs(1));
+
     }
 }
