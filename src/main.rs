558792bd9c521333177a7f67a464a3c25e917408
Tim Yorke <tim.yorke@embeddedscience.co.uk>
2021-01-02 20:13:50 +0000

    toggles GPIO pin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use rppal::gpio::Gpio;
+use std::{error::Error, thread::sleep, time::Duration};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut pin = Gpio::new()?.get(20)?.into_output();
+
+    loop {
+        pin.set_high();
+        sleep(Duration::from_millis(10));
+        pin.set_low();
+        sleep(Duration::from_millis(90));
+    }
 }
