14aa5a2b6d67a99b7ad2e41002e7d5a930ec15fa
Tim Yorke <tim.yorke@embeddedscience.co.uk>
2021-01-03 18:33:07 +0000

    rust/I2C: got stuck when I realised I2C0 is not covered by rppal

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,9 @@
-use rppal::{gpio::Gpio, pwm};
+use rppal::{gpio::Gpio, pwm, i2c::I2c};
 use rppal::pwm::{Channel, Polarity, Pwm};
 use std::{error::Error, thread::sleep, time::Duration};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut pin = Gpio::new()?.get(20)?.into_output();
-    loop {
-        pin.set_pwm(
-            Duration::from_micros(100),
-            Duration::from_micros(25)
-        )?;
-
-        sleep(Duration::from_secs(1));
-
-        pin.set_pwm(
-            Duration::from_micros(100),
-            Duration::from_micros(50)
-        )?;
-
-        sleep(Duration::from_secs(1));
-
-    }
+    let gpio = Gpio::new()?;
+//    let pin_scl = gpio.get(1)?.into
+    let i2c = I2c::new()?;
 }
