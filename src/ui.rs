054db22d7971e43b63d871791f121b71ab05643a
Tim Yorke <tim.yorke@embeddedscience.co.uk>
2021-02-21 15:47:44 +0000

    now with non-heap DisplayBuffer

--- src/ui.rs
+++ src/ui.rs
@@ -10,40 +10,39 @@ const FONT_MEDIUM : Font12x16 = Font12x16;
 
 const GREY: Rgb565 = Rgb565::new(12, 24, 12);
 
+
+
 #[derive(Clone)]
-struct DisplayBuffer {
-    buffer: Vec<u8>,
-    width: usize,
-    height: usize,
+struct DisplayBuffer<const WIDTH: usize, const HEIGHT: usize> 
+where [u8; WIDTH * HEIGHT * 2]: Sized
+{
+    buffer: [u8; WIDTH * HEIGHT * 2],
 }
 
-impl DisplayBuffer {
-    fn new(width: usize, height: usize) -> Self {
-        let buffer: Vec<u8> = vec![0u8; width * height * 2];
-        DisplayBuffer { buffer: buffer, width: width, height: height }
-    }
-}
-
-impl DrawTarget<Rgb565> for DisplayBuffer {
+impl<const WIDTH: usize, const HEIGHT: usize> DrawTarget<Rgb565> for DisplayBuffer<WIDTH, HEIGHT> 
+where [u8; WIDTH * HEIGHT * 2]: Sized
+{
     type Error = u32;
 
     fn draw_pixel(&mut self, item: Pixel<Rgb565>) -> Result<(), Self::Error> {
         let colour : u16 = item.1.into_storage();
         let col = item.0.x as usize;
         let row = item.0.y as usize; 
-        self.buffer[(col * 2) + (row * self.width)] = ((colour >> 8) & 0xFF) as u8;
-        self.buffer[(col * 2) + 1 + (row * self.width)] = (colour & 0xFF) as u8;
+        self.buffer[(col * 2) + (row * WIDTH)] = ((colour >> 8) & 0xFF) as u8;
+        self.buffer[(col * 2) + 1 + (row * WIDTH)] = (colour & 0xFF) as u8;
         Ok(())
     }
 
     fn size(&self) -> Size {
-        Size::new(self.width as u32, self.height as u32)
+        Size::new(WIDTH as u32, HEIGHT as u32)
     }
 }
 
-impl Drawable<Rgb565> for DisplayBuffer {
+impl<const WIDTH: usize, const HEIGHT: usize> Drawable<Rgb565> for DisplayBuffer<WIDTH, HEIGHT> 
+where [u8; WIDTH * HEIGHT * 2]: Sized
+{
     fn draw<D: DrawTarget<Rgb565>>(self, display: &mut D) -> Result<(), D::Error> {
-        let raw_image: ImageRaw<Rgb565> = ImageRaw::new(&self.buffer, self.width as u32, self.height as u32);
+        let raw_image: ImageRaw<Rgb565> = ImageRaw::new(&self.buffer, WIDTH as u32, HEIGHT as u32);
         let image: Image<_, Rgb565> = Image::new(&raw_image, Point::zero());
         image.draw(display)?;
         Ok(()) 
@@ -53,14 +52,13 @@ impl Drawable<Rgb565> for DisplayBuffer {
 
 
 pub struct Ui {
-    display: ST7789<SPIInterfaceNoCS<Spi, OutputPin>, OutputPin>,
-    draw_buffer: DisplayBuffer,
+    display: ST7789<SPIInterfaceNoCS<Spi, OutputPin>, OutputPin>
 }
 
 impl Ui {
     pub fn new(mut display: ST7789<SPIInterfaceNoCS<Spi, OutputPin>, OutputPin>) -> Self {
         display.clear(Rgb565::BLACK).unwrap();
-        let mut ui = Ui{display, draw_buffer: DisplayBuffer::new(57, 75)};
+        let mut ui = Ui{display};
         ui.draw_data_labels();
         ui
     }
@@ -98,14 +96,14 @@ impl Ui {
         let font = FONT_MEDIUM;
         let colour = Rgb565::new(Rgb565::MAX_R/2,Rgb565::MAX_G,Rgb565::MAX_B / 4);
         //let location = Point::new(x, 50);
-        let blank = Rectangle::new(Point::zero(), Point::new(57, 75))
-            .into_styled(PrimitiveStyle::with_fill(Rgb565::RED));
-        blank.draw(&mut self.draw_buffer).unwrap();
-        Ui::draw_text_at(format!("{:+.2}",q.v.x).as_str(), Point::new(0, 0), font, colour,&mut self.draw_buffer);
-        Ui::draw_text_at(format!("{:+.2}",q.v.y).as_str(), Point::new(0, 20), font, colour, &mut self.draw_buffer);
-        Ui::draw_text_at(format!("{:+.2}",q.v.z).as_str(), Point::new(0, 40), font, colour, &mut self.draw_buffer);
-        Ui::draw_text_at(format!("{:+.2}",q.s).as_str(), Point::new(0, 60), font, colour, &mut self.draw_buffer); 
-        // TODO: Remove the clone below:
-        self.draw_buffer.clone().draw(&mut self.display).unwrap();
+        let mut draw_buffer: DisplayBuffer<57, 75> = DisplayBuffer{ buffer: [0; 57 * 75 * 2]};
+        //let blank = Rectangle::new(Point::zero(), Point::new(57, 75))
+        //    .into_styled(PrimitiveStyle::with_fill(Rgb565::RED));
+        //blank.draw(&mut draw_buffer).unwrap();
+        Ui::draw_text_at(format!("{:+.2}",q.v.x).as_str(), Point::new(0, 0), font, colour,&mut draw_buffer);
+        Ui::draw_text_at(format!("{:+.2}",q.v.y).as_str(), Point::new(0, 20), font, colour, &mut draw_buffer);
+        Ui::draw_text_at(format!("{:+.2}",q.v.z).as_str(), Point::new(0, 40), font, colour, &mut draw_buffer);
+        Ui::draw_text_at(format!("{:+.2}",q.s).as_str(), Point::new(0, 60), font, colour, &mut draw_buffer); 
+        draw_buffer.draw(&mut self.display).unwrap();
     }
 }
